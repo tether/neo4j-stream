d7d27e1f848a62a05fe05a82b2f70d4e91ba65cf
bredele <olivier.wietrich@gmail.com>
2017-06-16 10:43:50 -0600

    Should run statement

--- test/neo4j-stream.js
+++ test/neo4j-stream.js
@@ -7,7 +7,7 @@ const test = require('tape')
 const stream = require('..')
 
 
-test('should create a session to run statatement', assert => {
+test('should create a session to run statement', assert => {
   assert.plan(1)
   const cypher = stream({
     session () {
@@ -15,4 +15,19 @@ test('should create a session to run statatement', assert => {
     }
   })
   cypher`MATCH (n) RETURN n`
+})
+
+test('should run a session statement', assert => {
+  assert.plan(1)
+  const statement = `MATCH (n) RETURN n`
+  const cypher = stream({
+    session () {
+      return {
+        run(str) {
+          assert.ok(str === statement, 'run statement')    
+        }
+      }
+    }
+  })
+  cypher`${statement}`
 })
